448ab3a62acbd3a6e124836712216db4502c2577
eduardoquea3 <eduardoquea3@gmail.com>
2026-01-04 21:32:59 -0500

    feat: integrate tanstack router
    - Add config for use tanstack router
    - Add first files of tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App";
-import "./index.css";
 
-const queryClient = new QueryClient();
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </React.StrictMode>,
-);
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
+}
